af5b1e80cc84b92ede1d6a2ca89a672e3cf96b0f
Virgil-Avram <avraam.virgil@yahoo.com>
2019-09-11 21:35:56 +0200

    Add header

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+
+  render() {
+
+    return (
+      <header>
+        <h1>Virgil Avram</h1>
+        <h2>Front-End Web Developer</h2>
+      </header>
+    )
+  }
+}
+
+export default Header
