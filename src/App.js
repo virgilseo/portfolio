238e4eb85c89059c6abda3667f89e0475aba2cf8
Virgil-Avram <avraam.virgil@yahoo.com>
2019-09-16 19:29:13 +0200

    Add contact section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import './App.css';
 import Header from './Header.js';
 import Footer from './Footer.js';
-import About from './About.js'
+import About from './About.js';
+import Contact from './Contact.js';
 
 class App extends Component {
 
@@ -12,6 +13,7 @@ class App extends Component {
       <div className='App'>
         <Header />
         <About />
+        <Contact />
         <Footer />
       </div>
     )
---
+++ src/Contact.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+
+  render() {
+
+    return(
+
+      <section>
+        <h3>Contact</h3>
+        <p>Social: </p>
+        <p>GitHub: </p>
+        <p>Mail: </p>  
+      </section>
+    )
+  }
+}
+
+export default Contact
