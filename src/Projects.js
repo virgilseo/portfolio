505e2d96b996d8ed5d0451d6f6719f93e666986c
Virgil-Avram <avraam.virgil@yahoo.com>
2019-10-24 10:19:07 +0200

    Add animation to project container

--- src/Projects.js
+++ src/Projects.js
@@ -10,14 +10,14 @@ class Projects extends Component {
         <h3>Projects</h3>
         <ul>
         {this.props.projects.map((project) => (
-          <li key={project.id} className='project'>
+          <li key={project.id} className={project.flipped === true ? 'project project-flipped' : 'project project-flipped-back'}>
             <div className={project.flipped === false  ? '' : 'hide'}>
               <h4>{project.title}</h4>
               <i>happy</i>
               <p>{project.tag}</p>
               <button onClick={() => this.props.flipProject(project.id)}>More details</button>
             </div>
-            <div className={project.flipped === true ? '' : 'hide'}>
+            <div className={project.flipped === true ? 'show' : 'hide'}>
               <button onClick={() => this.props.flipProject(project.id)}>Back</button>
               <p>{project.description}</p>
               <a href={project.gitLink}>View Code</a>
