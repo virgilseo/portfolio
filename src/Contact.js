9c62f4f1a343ae72c23b9f7327d24a172cc66cbc
Virgil-Avram <avraam.virgil@yahoo.com>
2019-11-03 16:11:41 +0100

    Update content in contact section

--- src/Contact.js
+++ src/Contact.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
+import LinkedinLogo from './Logo/LI-Logo.png';
+import GithubLogo from './Logo/GitHub_Logo_White.png'
 
 class Contact extends Component {
 
   render() {
-
+    console.log(LinkedinLogo)
     return(
 
       <section id='contact'>
         <h3 className='section-title'>Contact</h3>
-        <p>Social: </p>
-        <p>GitHub: </p>
-        <p>Mail: </p>
+        <a href='https://www.linkedin.com/in/virgil-avram/' className='profile-links' target="_blank" rel='noopener noreferrer'><img src={LinkedinLogo} alt='Linkedin-profile' className='social-logo'/></a>
+        <a href='https://github.com/virgilseo' className='profile-links' target="_blank" rel='noopener noreferrer'><img src={GithubLogo} alt='Github-profile' className='social-logo'/></a>
+        <div className='email-container'>
+          <i className='material-icons'>email</i>
+          <span className='email-text'>virgil.avram@gmail.com</span>
+        </div>
       </section>
     )
   }
