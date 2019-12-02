5053ab331c057d08fd73644270ffe377865bd00f
Virgil-Avram <avraam.virgil@yahoo.com>
2019-12-02 21:23:08 +0100

    Update content in about section

--- src/About.js
+++ src/About.js
@@ -8,16 +8,12 @@ class About extends Component {
     return (
 
        <section id='about'>
-       <h3 className='section-title'>About me</h3>
-       <img src={ProfilePic} alt='Profile' className='profile-pic'/>
-       <p className='bio-text'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus suscipit enim nec lacus luctus rhoncus.
-           Sed finibus volutpat neque, tempor congue velit finibus non. Sed ut sagittis massa.
-           Morbi bibendum, libero at vestibulum tempus, augue augue tincidunt magna, nec dignissim diam ex eu enim.
-           Phasellus et augue iaculis, ornare lacus nec, varius nibh. Quisque vitae elit vel enim luctus vestibulum.
-           Vivamus condimentum, elit sit amet eleifend feugiat, justo est dignissim felis, nec feugiat felis dolor lobortis magna.
-           Suspendisse ut arcu quis tellus accumsan porta. Proin ipsum leo, hendrerit elementum neque vitae, suscipit sollicitudin sem.
-           Maecenas quis tellus mauris. Fusce suscipit facilisis quam, nec fermentum turpis.
-           Etiam odio risus, interdum eu tellus sed, maximus mollis massa.</p>
+         <h3 className='section-title'>About me</h3>
+         <img src={ProfilePic} alt='Profile' className='profile-pic'/>
+         <p className='bio-text'> Detail-orientated front end web developer
+            with a proven background in search engine optimization and affiliate marketing.
+            Aiming to leverage my skills to build visually appealing, bug free and secure
+            web based applications.</p>
       </section>
     )
   }
