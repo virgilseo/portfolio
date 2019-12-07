c0e18062b964f1c7aa3e1b99af8e335c9a3d1b72
Virgil-Avram <avraam.virgil@yahoo.com>
2019-12-07 21:09:56 +0100

    Add new content to the footer

--- src/Footer.js
+++ src/Footer.js
@@ -7,6 +7,7 @@ class Footer extends Component {
     return(
       <footer className='footer-container'>
         <p>© 2019 Virgil Avram</p>
+        <p>Icons provided by <a href='https://material.io/' className='attr-link' target="_blank" rel='noopener noreferrer'>Material</a> and <a href='https://www.iconfinder.com/' className='attr-link' target="_blank" rel='noopener noreferrer'>IconFinder</a></p>
       </footer>
     )
   }
