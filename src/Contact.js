48275751461310053413b9398e523d9055ccff9b
Virgil-Avram <avraam.virgil@yahoo.com>
2019-12-11 22:49:11 +0100

    Update style for contact section

--- src/Contact.js
+++ src/Contact.js
@@ -1,20 +1,28 @@
 import React, {Component} from 'react';
 import LinkedinLogo from './Logo/LI-Logo.png';
-import GithubLogo from './Logo/GitHub_Logo_White.png'
+import GithubLogo from './Logo/GitHub-Logo-White.png';
 
 class Contact extends Component {
 
   render() {
-    
+
     return(
 
       <section id='contact'>
         <h3 className='section-title'>Contact</h3>
-        <a href='https://www.linkedin.com/in/virgil-avram/' className='profile-links' target="_blank" rel='noopener noreferrer'><img src={LinkedinLogo} alt='Linkedin-profile' className='social-logo'/></a>
-        <a href='https://github.com/virgilseo' className='profile-links' target="_blank" rel='noopener noreferrer'><img src={GithubLogo} alt='Github-profile' className='social-logo'/></a>
-        <div className='email-container'>
-          <i className='material-icons'>email</i>
-          <span className='email-text'>virgil.avram@gmail.com</span>
+        <div className='contact-container'>
+          <div className='linkedin-container'>
+            <img src={LinkedinLogo} alt='Linkedin-profile' className='social-logo'/>
+            <a href='https://www.linkedin.com/in/virgil-avram/' className='logo-links' target="_blank" rel='noopener noreferrer'>View profile</a>
+          </div>
+          <div className='git-container'>
+            <img src={GithubLogo} alt='Github-profile' className='social-logo'/>
+            <a href='https://github.com/virgilseo' className='logo-links' target="_blank" rel='noopener noreferrer'>View profile</a>
+          </div>
+          <div className='email-container'>
+            <i className='material-icons email-icon'>email</i>
+            <a href='mailto:virgilavram.dev@gmail.com' className='email-text'>virgilavram.dev@gmail.com</a>
+          </div>
         </div>
       </section>
     )
