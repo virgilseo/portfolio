dfd7a3a2cac2f7fcc658078bdd18679426fce502
Virgil-Avram <avraam.virgil@yahoo.com>
2020-01-23 21:57:33 +0100

    Fix position issue with project buttons

--- src/Projects.js
+++ src/Projects.js
@@ -12,14 +12,20 @@ class Projects extends Component {
         {this.props.projects.map((project) => (
           <li key={project.id} className={project.flipped === true ? 'project project-flipped' : 'project project-flipped-back'}>
             <div className={project.flipped === false  ? '' : 'hide'}>
-              <h4 className='project-title'>{project.title}</h4>
-              <img src={project.image} alt={project.title} className='project-image'/>
-              <p>{project.tag}</p>
-              <button className='project-btn' onClick={() => this.props.flipProject(project.id)}>More details</button>
+              <div className='content-container'>
+                <h4 className='project-title'>{project.title}</h4>
+                <img src={project.image} alt={project.title} className='project-image'/>
+                <p>{project.tag}</p>
+              </div>
+              <div className='button-container'>
+                <button className='project-btn' onClick={() => this.props.flipProject(project.id)}>More details</button>
+              </div>
             </div>
             <div className={project.flipped === true ? 'show' : 'hide'}>
-              <i className='material-icons back-btn' onClick={() => this.props.flipProject(project.id)}>arrow_back</i>
-              <p className='project-tags'>{project.description}</p>
+              <div className='flip-content-container'>
+                <i className='material-icons back-btn' onClick={() => this.props.flipProject(project.id)}>arrow_back</i>
+                <p className='project-tags'>{project.description}</p>
+              </div>
               <div className='links-container'>
                 <a className='project-links' href={project.gitLink} target="_blank" rel='noopener noreferrer'>View Code</a>
                 <a className='project-links' href={project.liveLink} target="_blank" rel='noopener noreferrer'>Live Page</a>
