8dd310e1487e9d8d79d2745289d8eb7a90adea5c
Virgil-Avram <avraam.virgil@yahoo.com>
2020-07-19 14:39:09 +0200

    Update style

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import LinkedinLogo from './Logo/LI-Logo.png';
 import GithubLogo from './Logo/GitHub-Logo-White.png';
+import GitLabLogo from './Logo/gitlab-icon-rgb.png';
 
 class Contact extends Component {
 
@@ -13,11 +14,15 @@ class Contact extends Component {
           <h4 className='contact-tag'>Get in touch</h4>
           <div className='linkedin-container'>
             <img src={LinkedinLogo} alt='Linkedin-profile' className='social-logo'/>
-            <a href='https://www.linkedin.com/in/virgil-avram/' className='logo-links' target="_blank" rel='noopener noreferrer'>View profile</a>
+            <a href='https://www.linkedin.com/in/virgil-avram/' className='logo-links' target="_blank" rel='noopener noreferrer'>Go to Linkedin</a>
           </div>
           <div className='git-container'>
             <img src={GithubLogo} alt='Github-profile' className='social-logo'/>
-            <a href='https://github.com/virgilseo' className='logo-links' target="_blank" rel='noopener noreferrer'>View profile</a>
+            <a href='https://github.com/virgilseo' className='logo-links' target="_blank" rel='noopener noreferrer'>Go to GitHub</a>
+          </div>
+          <div className='git-container'>
+            <img src={GitLabLogo} alt='Github-profile' className='social-logo lab-logo'/>
+            <a href='https://gitlab.com/virgilavram.dev' className='logo-links' target="_blank" rel='noopener noreferrer'>Go to GitLab</a>
           </div>
           <div className='email-container'>
             <i className='material-icons email-icon'>email</i>
